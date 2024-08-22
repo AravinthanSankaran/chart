f52348135282b3cbd9bd028c808b4090d76dfd7f
aravinthansankaran <aravinthanrsnkl@gmail.com>
2024-08-22 17:11:34 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Chart from './chart'
+import Chart2 from "./chart2"
+
+export default function Home() {
+  return (
+    <main className="flex flex-row w-full">
+   <Chart />
+   <Chart2 />
+    </main>
+  );
+}
